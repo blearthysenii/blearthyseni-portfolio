3c5d589ee6e3b52e5b6599321b68594588724d21
Bleart Hyseni <bleart.hyseni@umib.net>
2026-06-26 00:34:17 +0200

    Update page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,12 +1,12 @@
-import { Footer } from "../components/layout/Footer";
-import { Navbar } from "../components/layout/Navbar";
-import { About } from "../components/sections/About";
-import { Certificates } from "../components/sections/Certificates";
-import { Contact } from "../components/sections/Contact";
-import { Experience } from "../components/sections/Experience";
-import { Hero } from "../components/sections/Hero";
-import { Projects } from "../components/sections/Projects";
-import { Skills } from "../components/sections/Skills";
+import { Footer } from "@/components/layout/Footer";
+import { Navbar } from "@/components/layout/Navbar";
+import { About } from "@/components/sections/About";
+import { Certificates } from "@/components/sections/Certificates";
+import { Contact } from "@/components/sections/Contact";
+import { Experience } from "@/components/sections/Experience";
+import { Hero } from "@/components/sections/Hero";
+import { Projects } from "@/components/sections/Projects";
+import { Skills } from "@/components/sections/Skills";
 
 export default function Home() {
   return (
