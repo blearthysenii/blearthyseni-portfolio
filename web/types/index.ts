42ffd6d8f330140a2c3442754cb445027068df32
Bleart Hyseni <bleart.hyseni@umib.net>
2026-06-25 23:47:37 +0200

    add index.ts

---
+++ web/types/index.ts
@@ -0,0 +1,18 @@
+export type SkillGroup = {
+  title: string;
+  items: string[];
+};
+
+export type Project = {
+  title: string;
+  description: string;
+  stack: string[];
+  href: string;
+};
+
+export type ExperienceItem = {
+  role: string;
+  company: string;
+  period: string;
+  description: string;
+};
