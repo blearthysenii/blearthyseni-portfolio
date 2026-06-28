73ff608643881b3ae4b6411dd86bb21e293a8fd8
Bleart Hyseni <bleart.hyseni@umib.net>
2026-06-28 14:54:56 +0200

    Update

--- web/app/robots.ts
+++ web/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.blearthyseni.com/sitemap.xml",
+    sitemap: "https://blearthyseni.com/sitemap.xml",
   };
 }
--- web/app/sitemap.ts
+++ web/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.blearthyseni.com";
+  const baseUrl = "https://blearthyseni.com";
 
   return [
     {
